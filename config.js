93fdf38dd227f98516b4b54b8b91157ad02c18ad
Spelled as Jianlong Chen in Mandarin <lonnychason@gmail.com>
2020-01-03 13:46:26 +0800

    Initialization

---
+++ config.js
@@ -0,0 +1,10 @@
+var config = {
+    lines: [
+		"Do I love you?",
+		"Oh, my God.",
+		"if your love were a grain of sand,",
+		"mine would be a universe of beaches.",
+    ],
+    date: "2019-03-24T09:00:00",
+    names: ["LaoChen", "XiaoAi"]
+}
